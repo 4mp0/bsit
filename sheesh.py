97484a2572c093f0d8dc4c7095a3bd96fb5fc4a4
4mp0 <dcsean10@gmail.com>
2024-11-21 04:51:40 -0800

    Update

--- sheesh.py
+++ sheesh.py
@@ -1,6 +1,30 @@
-class user: #class blueprint for variables, user class name
-    string: str = "Hello" #string variable, str data type for variable, Hello value
-
-def main(): #def create function, main function name, () parameter
-    print(user.string) #user calling declared class, string getting the declared variable 
+class user: #class = blueprint for variables, user = class name
+    data: dict[str, int] = {}
+    push: list[str, int] = []
+    string: str = "" #string = variable, str = data type for variable, Hello = value
+    integer: int = 0
+    
+def main(): #def = create function, main = function name, () = parameter
+    
+    def reg():
+        userinput = input("input name: ") #userinput = variable, () = print before input
+        user.string = userinput #user = calling declared class, string = calling the variable inside the class, (variable = input) = make variable same as the input
+        userinput = input("input age: ")
+        user.integer = userinput
+        if len(userinput) != 0:
+            user.push.append(user.string); user.push.append(user.integer)
+        else:
+            print("error")
+    reg()             
+    
+    i = 0
+    while i < len(user.push):
+        if i == 0:        
+            user.data["name"] = user.push[i]
+        elif i == 1:            
+            user.data["age"] = user.push[i]
+        i += 1
+    
+    print(user.data) #user = calling declared class, string = calling the variable
 main() # main() #calling declared function to run or start
+
