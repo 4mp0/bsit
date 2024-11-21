162bce0fa07b769b819ea4cebccf78e25f0b63e7
4mp0 <dcsean10@gmail.com>
2024-11-21 02:26:58 -0800

    Update

--- sheesh.py
+++ sheesh.py
@@ -1,1 +1,6 @@
+class user: #class blueprint for variables, user class name
+    string: str = "Hello" #string variable, str data type for variable, Hello value
 
+def main(): #def create function, main function name, () parameter
+    print(user.string) #user calling declared class, string getting the declared variable 
+main() # main() #calling declared function to run or start
